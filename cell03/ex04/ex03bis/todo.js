ef9e779b96a5d9214c9a7d2731431e746737fa47
Siasa <84365286+siasa18037@users.noreply.github.com>
2024-09-12 21:51:31 +0700

    Update todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   const $ftList = $('#ft_list');
   const $newBtn = $('#newBtn');
-
+  
   const savedTodos = getCookie('todoList');
+
   if (savedTodos) {
-    const todos = JSON.parse(savedTodos);
+    const todos = JSON.parse(savedTodos).reverse();
     $.each(todos, function(index, todo) {
       addTodoToList(todo);
     });
@@ -41,9 +42,11 @@ $(document).ready(function() {
   }
 
   function getCookie(name) {
-    const value = `; ${document.cookie}`;
+    const value = `; ${document.cookie}`; 
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
     return '';
   }
+  
+
 });
